1210998769e5fff349dd7d2df482588f53b8d727
jackiem2 <jacqueline.merlan@gmail.com>
2022-04-25 20:12:02 -0500

    added working key

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,12 +1,3 @@
-var getWeather = function(city) {
-    var apiKey = "86ca01d3351656f207026dbba41aabc2"
-    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-
-    fetch(apiURL)
-    .then(function(response){
-        response.json().then(function(data){
-            displayWeather(data, city);
-        })
-    })
-}
-
+var cityList =$("#city-list");
+var cities = [];
+var key = "a7672c0c57d353373ddc2948e6599683"
